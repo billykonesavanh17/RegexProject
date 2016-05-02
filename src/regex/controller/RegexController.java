a9c989604298337ae81ea9ecf0163f4193075335
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-05-02 08:09:57 -0600

    Added view

--- src/regex/controller/RegexController.java
+++ src/regex/controller/RegexController.java
@@ -1,14 +1,33 @@
 package regex.controller;
 
+import regex.view.RegexView;
+import regex.view.RegexFrame;
+
 public class RegexController
 {
+	private RegexView myDisplay;
+	private RegexFrame baseFrame;
+	
+	
 	public RegexController()
 	{
+		myDisplay = new RegexView();
+		baseFrame = new RegexFrame();
 		
 	}
 	
 	public void start()
 	{
 		
+	}
+	
+	public RegexView getRegexView()
+	{
+		return myDisplay;
+	}
+	
+	public RegexFrame getBaseFrame()
+	{
+		return baseFrame;
 	}
 }
